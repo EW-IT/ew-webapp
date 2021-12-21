da4c8f17e8189ca417efc712b7dc72efa4edd157
TohDeKai <tohdekai@outlook.com>
2021-12-21 12:15:15 +0800

    Submit CSV buttonss

--- my-app/src/pages/Submit_csv.js
+++ my-app/src/pages/Submit_csv.js
@@ -1,13 +1,35 @@
+import { AutoAwesome } from '@mui/icons-material';
 import React from 'react';
 import Navbar from '../components/Navbar';
+
 
 const Submit_csv = () => {
     return (
         <>
         <Navbar />
-    <div>
+    <div  style={{
+          display: "flex",
+          justifyContent: "center"}}>
         <h3>This page is for JCRC Members to submit csv files</h3>
     </div>
+    <div  style={{
+          display: "flex",
+          justifyContent: "center"
+          }}>
+                <button className="stylised_button" style = {{
+                    marginRight: 10
+                }}>
+                    Browse Files
+                </button>
+                <button className="stylised_button" style = {{
+                    marginLeft: 10
+                }}>
+                    Submit CSV
+                </button>
+    </div>
+    
+    
+    
     </>
     );
 }
